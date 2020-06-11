d78e351d3aa916aaa0b3ae9327c293b56fbb0208
Christopher Kim <cj3kim@gmail.com>
2020-06-10 21:31:34 -0700

    completed 8.3.6

--- exp/main.go
+++ exp/main.go
@@ -21,40 +21,27 @@ func main() {
 		panic(err)
 	}
 
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	/* _, err = db.Exec(`INSERT INTO users(name, email) VALUES($1,$2)`, "Jon Calhoun", "jon@calhoun.io")*/
-	//if err != nil {
-	//panic(err)
-	//}
-
-	/* var id int*/
-	//row := db.QueryRow(`INSERT INTO users(name, email) VALUES($1,$2) RETURNING id`, "Christopher Kim", "chris@cj3kim.com")
-	//err = row.Scan(&id)
-	//if err != nil {
-	//panic(err)
-	//}
-
 	var id int
-	var name, email string
-	rows, err := db.Query(`
-		SELECT id, name, email
-		FROM users
-		WHERE email = $1
-		OR ID > $2`,
-		"chris@cj3kim.com", 2)
+	for i := 1; i < 6; i++ {
+		// Create some fake data
+		userId := 1
+		if i > 3 {
+			userId = 2
+		}
 
-	if err != nil {
-		panic(err)
+		amount := 1000 * i
+		description := fmt.Sprintf("USB-C Adapter x%d", i)
+
+		err = db.QueryRow(`
+			INSERT INTO orders (user_id, amount, description)
+			VALUES ($1,$2,$3)
+			RETURNING id`,
+			userId, amount, description).Scan(&id)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Created an order with the ID:", id)
 	}
-
-	for rows.Next() {
-		rows.Scan(&id, &name, &email)
-		fmt.Println("ID:", id, "Name:", name, "Email:", email)
-	}
-
 	db.Close()
 }
